ddc0f1a2172e3f28e9ec593a95eb751325cbf8ba
Dinis <denis.dr4@gmail.com>
2018-10-16 02:27:50 +0100

    Started main file, does 2.2/2.3/2.4

--- main.m
+++ main.m
@@ -27,9 +27,30 @@ function main
     ylabel('deviation ','Interpreter', 'Latex')
 %% 2.3
 %
-clear all
+
     for i=1:7
        [c, d] = variationA(lambda(i), 1,2);
+       count(i)=c;
+       dev(i)=d;
+    end
+    %plot number of times u(t) changes for all lambdas
+    figure
+    bar(count)
+    title('Times u(t) has changed', 'Interpreter', 'Latex')
+    xlabel('$ \lambda $','Interpreter', 'Latex')
+    ylabel('number ','Interpreter', 'Latex')
+    
+    %plot deviation for all lambdas
+    figure
+    bar(dev)
+    title('Mean Deviation', 'Interpreter', 'Latex')
+    xlabel('$ \lambda $','Interpreter', 'Latex')
+    ylabel('deviation ','Interpreter', 'Latex')
+    
+%% 2.4
+%    
+    for i=1:7
+       [c, d] = variationA(lambda(i), 1,1);
        count(i)=c;
        dev(i)=d;
     end
